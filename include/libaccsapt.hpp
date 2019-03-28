83c7d171bb506c6af2e86cb921fa1b8760d3f338
bryce_workmac <westheb@iastate.edu>
2019-03-28 09:41:49 -0500

    More skeleton structure for copying over files when possible

---
+++ include/libaccsapt.hpp
@@ -0,0 +1,16 @@
+/*
+ * libaccsapt.hpp
+ * 
+ * Copied: Mar 28, 2019
+ * Author: Bryce M. Westheimer
+ * 
+ * Description: Header collection file
+ * 
+ */
+
+#ifndef LIBACCSAPT_LIBACCSAPT_HPP_
+#define LIBACCSAPT_LIBACCSAPT_HPP_
+
+// TODO: Include headers present in public version
+
+#endif /* LIBACCSAPT_LIBACCSAPT_HPP_ */
